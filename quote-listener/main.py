7f1bb19498f470e33bd3429b3d3a91e0dfc80532
cyril.carlier <cyrilcalier@hotmail.com>
2020-09-17 16:43:35 +0200

    Init

--- quote-listener/main.py
+++ quote-listener/main.py
@@ -1,5 +1,12 @@
 import praw
 
 if __name__ == '__main__':
+
+    reddit = praw.Reddit(client_id="my client id",
+                     client_secret="my client secret",
+                     user_agent="my user agent")
+
     while True:
-        #   todo
+        for comment in reddit.subreddit("all").stream.comments(skip_existing=True):
+            print(comment)
+            #todo
